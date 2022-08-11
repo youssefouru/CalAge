4534655e028dfda35b7067bedeee367902db11bc
Amine Youssef <youssef.amine@epfl.ch>
2022-08-11 18:59:50 +0200

    update

--- src/DiagChamber.py
+++ src/DiagChamber.py
@@ -23,16 +23,17 @@ class DiagChamber:
         self.temperature = temperature
         self.time = relativedelta(days=time)
         self.channels = {Fr(i): channels[i] for i in range(0, len(channels))}
-        self.loaded_batteries = {x: [] for x in Fr}
+        self.loaded_batteries = {}
         self.sealed = False
         self.finish_date = date.today() + self.time
 
-    def load_battery(self, battery,channel):
+    def load_battery(self, battery, channel):
         """
         This method will load the battery into diagnostic.
 
         Parameters
         ----------
+        :param channel: The channel assigned to the battery
         :param battery:  The battery we want to register.
         :return: error code.
         """
@@ -48,9 +49,8 @@ class DiagChamber:
     def start_diagnostic(self):
         self.finish_date = date.today() + self.time
         self.sealed = True
-        for (shape, batteries) in self.loaded_batteries.items():
-            for battery in batteries:
-                battery.under_diag = True
+        for (slot, battery) in self.loaded_batteries.items():
+            battery.under_diag = True
 
     def isFinished(self):
         return date.today() >= self.finish_date
@@ -59,16 +59,16 @@ class DiagChamber:
         if not (self.isFinished() or aborted):
             return Err.ERR_SEALED_CHAMBER
         print("unloading")
-        for (shape, batteries) in self.loaded_batteries.items():
-            for battery in batteries:
-                battery.under_diag = False
-                if not aborted:
-                    battery.diagnostic_number += 1
-                    battery.next_diag = date.today() + battery.diagnostic_frequency
-                if aborted:
-                    battery.next_diag = date.today()
-            self.channels[shape] += len(batteries)
-            self.loaded_batteries[shape] = []
+        for (slot, battery) in self.loaded_batteries.items():
+            battery.under_diag = False
+            if not aborted:
+                battery.diagnostic_number += 1
+                battery.next_diag = date.today() + battery.diagnostic_frequency
+            if aborted:
+                battery.next_diag = date.today()
+            self.channels[battery.form_factor] += 1
+
+        self.loaded_batteries = {}
         self.sealed = False
 
     def advance_time(self, time=1):
@@ -81,7 +81,8 @@ class DiagChamber:
 
     def toString(self):
         print("Diagnostic Chamber:")
-        numBat = 0
-        for (shape, batteries) in self.loaded_batteries.items():
-            numBat += len(batteries)
+        numBat = len(self.loaded_batteries)
         print("Temperature={}, Time={} days, Batteries={}".format(self.temperature, self.time.days, numBat))
+        for (channel, battery) in self.loaded_batteries.items():
+            print("Slot {}:".format(channel))
+            battery.toString()
